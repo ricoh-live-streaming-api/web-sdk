bb4fcb931a069e0ac8276910be168292c12c85a0
demizukaori <53554808+demizukaori@users.noreply.github.com>
2022-01-26 12:05:35 +0900

    Update ricoh-ls-sdk.d.ts

--- src/ricoh-ls-sdk.d.ts
+++ src/ricoh-ls-sdk.d.ts
@@ -1,6 +1,6 @@
 /** @module ricoh-ls-sdk
  */
-declare module "@ricoh-live-streaming-api/ricoh-ls-sdk" {
+declare module "@ricoh-live-streaming-api/web-sdk" {
   /** @typedef {String} ConnectionID
    */
   type ConnectionID = string;
